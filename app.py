6f3f7e5d5afa21c154ba392e2c1db1ed3bcc4f68
Daniel Felipe Caro Torres <danielcaro317@gmail.com>
2026-01-28 15:07:55 -0500

    Comment done

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import datetime as dt
 
 
-
+# changes after published branch
 app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
